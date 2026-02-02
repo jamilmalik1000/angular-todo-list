32743c5815d630f3c3ea9228b5da0e2dce6e86fc
Jamil Malik <jamilmalik2500@gmail.com>
2026-02-02 11:02:51 +0500

    Initial commit: Angular To-do list with Edit/Delete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Todolist');
+  task='';
+  tasklist:{name:string}[]=[];
+ addtask() {
+  this.task = this.task.trim(); 
+  if (this.task) { 
+    this.tasklist.push({ name: this.task }); 
+    this.task = ''; 
+  }
+}
+deletetask(index:number)
+{
+  this.tasklist.splice(index,1)
+}
+editbutton(index: number) {
+
+  this.task = this.tasklist[index].name;
+
+  this.deletetask(index);
+}
+
 }
